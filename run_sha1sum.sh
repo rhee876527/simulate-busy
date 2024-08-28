b9d9636b6a971b3b8f87f3916f82824a78b02008
rhee876527 <26869897+rhee876527@users.noreply.github.com>
2024-08-29 02:35:59 +1000

    Add some pizzazz to script

--- run_sha1sum.sh
+++ run_sha1sum.sh
@@ -3,7 +3,11 @@
 TIMEOUT_SECONDS="${TIMEOUT_SECONDS:-30}"
 INTERVAL_SECONDS="${INTERVAL_SECONDS:-7200}"
 
+echo -e "\033[1;33mScript started. Locking one CPU thread for the next $TIMEOUT_SECONDS seconds.\033[0m"
+
 while true; do
+  echo -e "\033[0;33mRunning sha1sum on /dev/zero...\033[0m"
   timeout "$TIMEOUT_SECONDS" sha1sum /dev/zero
+  echo -e "\033[0;32mSha1sum completed. Sleeping for $INTERVAL_SECONDS seconds before next run.\033[0m"
   sleep "$INTERVAL_SECONDS"
 done
